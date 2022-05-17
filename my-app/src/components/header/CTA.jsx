c4d93e232c6220d4d852624c31be3fbed5131aae
Piorek1994 <pprus94@gmail.com>
2022-05-17 17:11:39 +0200

    Header jpg

---
+++ my-app/src/components/header/CTA.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CTA = () => {
+  return (
+    <div className="cta">
+        <a href="" download>Download CV</a>
+        <a href="#contact">Contact</a>
+    </div>
+  )
+}
+
+export default CTA
--- my-app/src/components/header/Header.jsx
+++ my-app/src/components/header/Header.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/me.jpg'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Piotr Prus</h1>
+        <h5 className='text-light'>I am future Front-end Developer</h5>
+        <CTA/>
+
+        <div className="me">
+          <img src={ME} alt="" />
+        </div>
+
+        <a href="#contact" className='scroll__down'>Scroll Down</a>
+
+      </div>
+    </header>
   )
 }
 
