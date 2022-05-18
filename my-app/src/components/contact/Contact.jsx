d64a37e6e0b65788a82d5e7f12e9572b162f73ce
Piorek1994 <pprus94@gmail.com>
2022-05-18 17:51:52 +0200

    Contact finished

--- my-app/src/components/contact/Contact.jsx
+++ my-app/src/components/contact/Contact.jsx
@@ -3,8 +3,25 @@ import './contact.css'
 import {MdOutlineEmail} from 'react-icons/md'
 import {RiMessengerLine} from 'react-icons/ri'
 import {BsWhatsapp} from 'react-icons/bs'
+import { useRef } from 'react';
+import emailjs from 'emailjs-com'
 
 const Contact = () => {
+  const form = useRef(); 
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_rwbcyx9', 'template_2ljj72m', form.current, '07AZLH0eV-nDl66PX')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset()
+  };
+
+
 	return (
 		<section id='contact'>
 			<h5>Get In Touch</h5>
@@ -32,7 +49,7 @@ const Contact = () => {
 
         </div>
 
-				<form action=''>
+				<form ref={form} onSubmit={sendEmail}>
           <input type="text" name='name' placeholder='Your Full Name' required />
           <input type="email" name='email' placeholder='Your Email' required />
           <textarea name="message"  rows="7" placeholder='Yor Message' required></textarea>
