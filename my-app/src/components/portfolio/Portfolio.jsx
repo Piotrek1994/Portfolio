c2199edbe1f952a1a47f96d1cba4e56529ac3197
Piorek1994 <pprus94@gmail.com>
2022-05-18 14:08:47 +0200

    Portfolio finished

--- my-app/src/components/portfolio/Portfolio.jsx
+++ my-app/src/components/portfolio/Portfolio.jsx
@@ -1,10 +1,59 @@
 import React from 'react'
 import './portfolio.css'
+import ME from '../../assets/me.jpg'
 
- const Portfolio = () => {
-  return (
-    <section id='portfolio'>Portfolio</section>
-  )
+
+const data = [
+  {
+    id: 1,
+    image: ME,
+    title: 'Title',
+    github: 'https://github.com',
+    demo: 'https://dribble.com'
+  },
+  {
+    id: 2,
+    image: ME,
+    title: 'Title',
+    github: 'https://github.com',
+    demo: 'https://dribble.com'
+  },
+  {
+    id: 3,
+    image: ME,
+    title: 'Title',
+    github: 'https://github.com',
+    demo: 'https://dribble.com'
+  }
+]
+
+const Portfolio = () => {
+	return (
+		<section id='portfolio'>
+			<h5>My Recent Work</h5>
+			<h2>Portfolio</h2>
+
+			<div className='container portfolio__container'>
+				{data.map(({id, image, title ,github, demo})=> {
+          return (<article key={id} className='portfolio__item'>
+					<div className='portfolio__item-image'>
+						<img src={image} alt={title} />
+					</div>
+					<h3>{title}</h3>
+					<div className='portfolio__item-cta'>
+						<a href={github} className='btn' target='_blank'>
+							Github
+						</a>
+						<a href={demo} className='btn btn-primary' target='_blank'>
+							Live Demo
+						</a>
+					</div>
+				</article>)
+        })}
+				
+			</div>
+		</section>
+	)
 }
 
 export default Portfolio
